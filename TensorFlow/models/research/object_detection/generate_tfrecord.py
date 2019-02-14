72f24aed376aab1dbd29fd8390e7876b18606177
SocioDroid <sociodroid2019@gmail.com>
2019-02-14 09:00:12 +0000

    DOne

--- TensorFlow/models/research/object_detection/generate_tfrecord.py
+++ TensorFlow/models/research/object_detection/generate_tfrecord.py
@@ -31,6 +31,8 @@ FLAGS = flags.FLAGS
 def class_text_to_int(row_label):
     if row_label == 'helmet':
         return 1
+    elif row_label == 'nohelmet':
+        return 2
     else:
         None
 
